a956977770c599e9012e1a639dc5f6fd19f69a36
Gokul <gokul@vthink.co.in>
2024-08-20 18:44:00 +0530

    20-08-2024

---
+++ self_join.sql
@@ -0,0 +1,51 @@
+use employees;
+-- show tables;
+-- select * from emp_manager;
+
+-- Task: From emp_manager table extract the  record data only of those employees who are managers as well.
+
+SELECT 
+  e1.*
+FROM
+   emp_manager e1
+   JOIN
+   emp_manager e2 ON e1.emp_no=e2.manager_no 
+   where
+   e2.emp_no IN (select
+   manager_no
+   FROM
+   emp_manager);
+select
+   manager_no
+   FROM
+   emp_manager;  
+ -- Select all rows from the emp_manager table to understand the information stored in it.
+
+-- Execute a query containing a self-join of the emp_manager table on the employee and manager numbers, 
+-- using the aliases e1 and e2. In the field list, designate all columns in the order they appear in the e2 table.  
+   
+   SELECT * FROM emp_manager;
+ 
+SELECT 
+    e2.*
+FROM
+    emp_manager e1
+        JOIN
+    emp_manager e2 ON e1.emp_no = e2.manager_no;
+    
+-- Execute a query containing a self-join of the emp_manager table on the employee and manager numbers, 
+-- using the aliases e1 and e2. Ensure the result set contains only unique rows. In the field list, 
+-- designate all columns in the order they appear in the e1 table.
+SELECT DISTINCT
+    e1.emp_no,
+    e1.dept_no,
+    e1.manager_no
+FROM
+    emp_manager e1
+JOIN
+    emp_manager e2
+ON
+    e1.manager_no = e2.emp_no
+    AND e1.dept_no = e2.dept_no;
+
+    
--- sub_query.sql
+++ sub_query.sql
@@ -1,3 +1,73 @@
+use employees;
+-- Extract the information about all department managers who were hired between the 1st of January 
+-- 1990 and the 1st of January 1995.
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
+            
+-- Use a subquery with an IN operator inside a WHERE clause to obtain the employee number (emp_no), 
+-- department number (dept_no), and contract start date (from_date) from the dept_manager table. 
+-- Retrieve only data about managers born in or after 1955.   
+SELECT emp_no, dept_no, from_date
+FROM dept_manager
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE birth_date >= '1955-01-01'
+);
+-- Select the entire information for all employees whose job title is “Assistant Engineer”. 
+
+-- Hint: To solve this exercise, use the 'employees' table.    
+SELECT 
+    *
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            titles t
+        WHERE
+            t.emp_no = e.emp_no
+                AND title = 'Assistant Engineer');
+                
+-- Use a subquery with EXISTS inside a WHERE clause of an outer query to retrieve all data from the salaries table.
+--  Specify in the subquery that you are only interested in individuals whose job title is 'Engineer'.
+select 
+*
+from salaries s
+where
+exists(select * 
+       from
+       titles t
+       where s.emp_no=t.emp_no
+       and title='Engineer')
+       order by emp_no;
+
+select 
+   emp_no
+   from
+   dept_manager
+   
+                
+
+
+
+
+
+
+
+
+
+
 -- Starting your code with “DROP TABLE”, create a table called “emp_manager” 
 -- (emp_no – integer of 11, not null; dept_no – CHAR of 4, null; manager_no – integer of 11, not null). 
 DROP TABLE IF EXISTS emp_manager;
@@ -33,10 +103,10 @@ CREATE TABLE emp_manager (
 -- Following the same logic, create subset D. Here you must do the opposite - assign employee 110022 as a manager to employee 110039.
 
 -- Your output must contain 42 rows.
-
-
-
-
+select * from employees;
+DELETE FROM emp_manager;
+select * from dept_manager;
+select * from dept_emp;
 INSERT INTO emp_manager
 SELECT 
     u.*
@@ -113,3 +183,44 @@ FROM
     WHERE
         e.emp_no = 110039
     GROUP BY e.emp_no) AS d) as u;
+    select * from emp_manager;
+   
+   --  Retrieve a table containing three columns:
+
+-- 1. The employee number (emp_no) from the titles table
+-- 2. The job title from the titles table
+-- 3. A column named avg_salary containing the average salary from the employees' records stored in the salaries table, rounded to the nearest cent. To obtain this column, use a subquery in the outer SELECT statement.
+
+-- To obtain the desired output, use a second subquery, embedded in the FROM clause of the outer query. In its field list, specify that you are only interested in the employee number (emp_no) and title (title) from the records stored in the titles table. In this subquery, you should also indicate that you are focusing on employees whose job title is 'Staff' or 'Engineer'.
+
+-- Sort your output by the average salary in descending order.
+
+SELECT 
+    t.emp_no,
+    t.title,
+    (SELECT 
+            ROUND(AVG(s.salary), 2)
+        FROM
+            salaries s
+        WHERE
+            s.emp_no = t.emp_no) AS avg_salary
+FROM
+    (SELECT 
+        emp_no, title
+    FROM
+        titles t
+    WHERE
+        title = 'Staff' OR title = 'Engineer') as t
+ORDER BY avg_salary DESC;
+
+select e.emp_no AS employee_ID,
+    min(de.dept_no) As department_code,
+    (select emp_no from dept_manager where emp_no='110022') AS manager_ID
+    from employees e
+    join
+    dept_emp de on de.emp_no=e.emp_no
+     WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    
+    
